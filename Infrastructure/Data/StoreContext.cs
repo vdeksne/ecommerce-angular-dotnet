78c95a3004a1e90004636051545b5393b354784e
Viktorija Deksne <102534473+vdeksne@users.noreply.github.com>
2025-03-04 13:33:55 +0200

    Postman and docker setup

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,22 @@
+using System;
+using Core.Entities;
+using Infrastructure.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class StoreContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+    // public DbSet<Address> Addresses { get; set; }
+    // public DbSet<DeliveryMethod> DeliveryMethods { get; set; }
+    // public DbSet<Order> Orders { get; set; }
+    // public DbSet<OrderItem> OrderItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+    }
+}
